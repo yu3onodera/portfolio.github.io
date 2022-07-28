b8edef1cea7fac4fd8330d5dbef3abf5137260a2
yu3onodera <110168748+yu3onodera@users.noreply.github.com>
2022-07-28 21:02:58 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+'use strict';
+
+{
+  // Intersection Observer API
+
+  function inViewCallback(entries, obs) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      }
+
+      entry.target.classList.add('appear');
+      obs.unobserve(entry.target);
+    });
+  }
+
+  function onScrollCallback(entries) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        toTop.classList.add('scrolled');
+      } else {
+        toTop.classList.remove('scrolled');
+      }
+    });
+  }
+
+  const toTop = document.getElementById('to_top');
+
+  const inViewObserver = new IntersectionObserver(inViewCallback, {
+    threshold: 0.2,
+  });
+
+  document.querySelectorAll('.animate').forEach(el => {
+    inViewObserver.observe(el);
+  });
+
+  toTop.addEventListener('click', e => {
+    e.preventDefault();
+
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  });
+}
